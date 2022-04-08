1edf47e032ae3f15d060d58518b1a91e8633f32b
Marko Janík <xjanikm@stuba.sk>
2022-04-08 17:26:36 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.janik {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.janik to javafx.fxml;
+    exports com.example.janik;
+}
